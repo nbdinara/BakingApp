d09a3528ad49add0d793953642f67438a00396de
diko <nbdinara@gmail.com>
2019-12-03 15:29:58 -0800

    activity_main.xml views and LinearLayoutManager were created and initialized in onCreate method of MainActivity.class.

--- app/src/main/java/com/example/bakingapp/MainActivity.java
+++ app/src/main/java/com/example/bakingapp/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.bakingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private RecyclerView mRecyclerView;
+    private ProgressBar mLoadingIndicator;
+    private TextView mErrorMessageDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = findViewById(R.id.rv_recipes);
+        mErrorMessageDisplay = findViewById(R.id.tv_error_message_display);
+        mLoadingIndicator = findViewById(R.id.pb_loading_all_recipes_indicator);
+
+        LinearLayoutManager layoutManager
+                = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+
+        mRecyclerView.setLayoutManager(layoutManager);
+
+
     }
 }
